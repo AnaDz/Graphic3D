5c5a3e780e820d7761dcb59cc2447b72e9aa218b
Matthieu Bélot <matthieu.belot@ensimag.grenoble-inp.fr>
2017-03-30 15:13:54 +0200

    Ca commence déjà mal

---
+++ project_skyrim_2.cpp
@@ -0,0 +1,83 @@
+#include "../include/ShaderProgram.hpp"
+#include "../include/Viewer.hpp"
+
+/* PRACTICAL_03 : RENDERABLES */
+#include "../include/FrameRenderable.hpp"
+#include "../teachers/CubeRenderable.hpp"
+#include "../teachers/IndexedCubeRenderable.hpp"
+#include "../teachers/CylinderRenderable.hpp"
+#include "../teachers/MeshRenderable.hpp"
+
+/* PRACTICAL_04 : LIGHTNING */
+#include "../include/lighting/DirectionalLightRenderable.hpp"
+#include "../include/lighting/PointLightRenderable.hpp"
+#include "../include/lighting/SpotLightRenderable.hpp"
+#include "../include/lighting/LightedCubeRenderable.hpp"
+#include "../include/lighting/LightedCylinderRenderable.hpp"
+#include "../include/lighting/LightedMeshRenderable.hpp"
+
+/* PRACTICAL_05 : TEXTURING */
+#include "../include/texturing/TexturedPlaneRenderable.hpp"
+#include "../include/texturing/TexturedCubeRenderable.hpp"
+#include "../include/texturing/MultiTexturedCubeRenderable.hpp"
+#include "../include/texturing/MipMapCubeRenderable.hpp"
+#include "../include/texturing/TexturedMeshRenderable.hpp"
+
+/* PRACTICAL_06 : KEYFRAMES */
+#include "../include/keyframes/KeyframedCylinderRenderable.hpp"
+#include "../include/keyframes/GeometricTransformation.hpp"
+
+/* PRACTICAL_07 : DYNAMICS */
+#include "../include/dynamics/DynamicSystem.hpp"
+#include "../include/dynamics/DampingForceField.hpp"
+#include "../include/dynamics/ConstantForceField.hpp"
+#include "../include/dynamics/SpringForceField.hpp"
+#include "../include/dynamics/EulerExplicitSolver.hpp"
+
+#include "../include/dynamics_rendering/DynamicSystemRenderable.hpp"
+#include "../include/dynamics_rendering/ParticleRenderable.hpp"
+#include "../include/dynamics_rendering/ParticleListRenderable.hpp"
+#include "../include/dynamics_rendering/ConstantForceFieldRenderable.hpp"
+#include "../include/dynamics_rendering/SpringForceFieldRenderable.hpp"
+#include "../include/dynamics_rendering/SpringListRenderable.hpp"
+#include "../include/dynamics_rendering/ControlledForceFieldRenderable.hpp"
+#include "../include/dynamics_rendering/QuadRenderable.hpp"
+
+/* NOS PROPRES CLASSES A INCLUDE ICI */
+
+void initialize_project_skyrim_2(Viewer& viewer) {
+
+  // Shaders
+  ShaderProgramPtr flatShader = std::make_shared<ShaderProgram>(
+      "../shaders/flatVertex.glsl", "../shaders/flatFragment.glsl");
+  ShaderProgramPtr phongShader = std::make_shared<ShaderProgram>(
+      "../shaders/phongVertex.glsl", "../shaders/phongFragment.glsl");
+  viewer.addShaderProgram(flatShader);
+  viewer.addShaderProgram(phongShader);
+
+  // Two texture shaders: simple and multi-texturing
+  ShaderProgramPtr texShader = std::make_shared<ShaderProgram>(
+    "../shaders/textureVertex.glsl", "../shaders/textureFragment.glsl");
+  viewer.addShaderProgram(texShader);
+  ShaderProgramPtr multiTexShader = std::make_shared<ShaderProgram>(
+    "../shaders/multiTextureVertex.glsl", "../shaders/multiTextureFragment.glsl");
+  viewer.addShaderProgram(multiTexShader);
+
+  // Frame
+  FrameRenderablePtr frame = std::make_shared<FrameRenderable>(flatShader);
+  viewer.addRenderable(frame);
+
+  fprintf(stderr, "Skyrim chargé !\n");
+
+  //Position the camera
+  viewer.getCamera().setViewMatrix(
+    glm::lookAt(
+      glm::vec3(0, -8, 8 ),
+      glm::vec3(0, 0, 0),
+      glm::vec3( 0, 0, 1 ) ) );
+
+  // Run the animation
+  viewer.setAnimationLoop(true, 6.0);
+  viewer.startAnimation();
+
+}
